f200f419e094d0538edb87f21d3437f456723bc3
Roman100500 <chizhkinra001@mail.ru>
2022-10-23 18:06:15 +0300

    added Task 19.2 (Array Method)

---
+++ Homework3/Task 19.2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход n-значное число и проверяет, является ли оно палиндромом.
+
+bool ArrayPalindrome(int y, string z)
+{
+    int[] array = new int[y];
+    string k;
+    bool m = true;
+    for (int i = 0; i < y; i++)
+    {
+        k = Convert.ToString(z[i]);
+        array[i] = Convert.ToInt32(k);
+        Console.Write(array[i] + " ");
+    }
+    Console.Write(" - array ");
+    Console.WriteLine();
+    for (int j = 0; j < y + 1 / 2; j++)
+    {
+        if(array[j] != array[y - 1 - j])
+        {
+            m = false;
+            break;
+        }
+    }
+    return m;
+}
+
+Console.Write("Input a natural number: ");
+string c = Console.ReadLine();
+int b =  c.Length;
+bool res = ArrayPalindrome(b, c);
+Console.WriteLine(res);
