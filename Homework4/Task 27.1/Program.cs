1df5b03beaa702368ea98b5da1c0a36d65b74eae
Roman100500 <chizhkinra001@mail.ru>
2022-11-08 21:30:21 +0300

    added Task 27.1 (foreach + double convert)

---
+++ Homework4/Task 27.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 27. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Ghost(string x)
+{
+    x = x.Trim(new char[] { ' ', '-' });
+    int l = 0;
+    foreach (char a in x)
+        l += Convert.ToInt32(Convert.ToString(a));
+    return l;
+}
+
+Console.Write("Input a number: ");
+string x = Console.ReadLine();
+Console.WriteLine($"The sum of the numbers: {Ghost(x)}");
